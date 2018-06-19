8ae736fa044d1e0f7f17d9a41aa27273ebd62633
bchang <bchang@fulcrm.com>
2018-06-19 15:54:49 +0800

    update files

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,10 +8,12 @@ export default Route.extend({
   },
   actions: {
     next(p) {
+      this.controller.set('event', 'toLeft');
       p++;
       this.transitionTo(`/?p=${p}`);
     },
     back(p) {
+      this.controller.set('event', 'toRight');
       p--;
       p = p > 0 ? p : 0;
       this.transitionTo(`/?p=${p}`);
---
+++ app/transitions.js
@@ -0,0 +1,18 @@
+export default function(){
+  // Add your transitions here, like:
+  //   this.transition(
+  //     this.fromRoute('people.index'),
+  //     this.toRoute('people.detail'),
+  //     this.use('toLeft'),
+  //     this.reverse('toRight')
+  //   );
+  this.transition(
+    this.hasClass('toLeft'),
+    this.use('toLeft')
+  );
+
+  this.transition(
+    this.hasClass('toRight'),
+    this.use('toRight')
+  );
+}
